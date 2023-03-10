c0c55eaebba1c8dd1850e2ceab4e48c67abfa1f1
Alvin0183 <alvinmartinez666@outlook.com>
2023-03-09 20:08:03 -0600

    V1

---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("Por favor, introduce un número entero:");
+Console.WriteLine("---------------------------------------");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("---------------------------------------");
+for (int i = 1; i <= numero; i++)
+{
+    for (int xd = 1; xd <= i; xd++)
+    {
+       Console.Write("*");
+    }
+       Console.WriteLine();
+}
+Console.WriteLine("---------------------------------------");
